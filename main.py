7fc5d76b547525d427f27ae83cbbd404b4550241
Alban <alban.xhaferllari@gmail.com>
2020-04-13 19:57:15 -0500

    added algo to calculate max porfit sum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ccxt
 import config
+import copy
 
 
 # NOTE: dynamically calculate max profit function
@@ -16,8 +17,8 @@ class Arbitrager:
             'theocean': ccxt.theocean(),
             # 'okex': ccxt.okex(),            #Canadian, does not allow us
             'bitmart': ccxt.bitmart(),
-            'cex': ccxt.cex(),              #EU
-            'bitbay': ccxt.bitbay(),        #EU
+            'cex': ccxt.cex(),  # EU
+            # 'bitbay': ccxt.bitbay(),  # EU, Updating API
             # 'bcex': ccxt.bcex(),            #candian exch, their API is updating
             'bitbay': ccxt.bitbay(),
             'paymium': ccxt.paymium(),
@@ -25,33 +26,34 @@ class Arbitrager:
             'okcoin': ccxt.okcoin(),
             'bitfinex': ccxt.bitfinex()      # non-US
         }
-        self.loadMarkets() #creates a markets variable in each exchange instance.  ex. exchages[0].markets will return markets
+        # creates a markets variable in each exchange instance.  ex. exchages[0].markets will return markets
+        self.loadMarkets()
         # these are tickers available on exchnage, but not US customers, or don't allow deposits/withdrawals
         self.unavailableTickers = {
             'binanceus': [],
-            'bittrex': ['LUNA/BTC', 'ABBC/BTC','Capricoin/BTC','DRGN/BTC','CVT/BTC','NXT/BTC'],
+            'bittrex': ['LUNA/BTC', 'ABBC/BTC', 'Capricoin/BTC', 'DRGN/BTC', 'CVT/BTC', 'NXT/BTC'],
             # 'coinbase': [],
             'gemini': [],
             'kraken': [],
-            'livecoin': ['BTM/BTC', 'BTM/ETH', 'NANO/BTC','NANO/ETH', 'XTZ/BTC', 'XTZ/ETH','THETA/BTC','THETA/ETH','ABBC/BTC','ABBC/ETH','AE/BTC','AE/ETH','IOST/BTC','IOST/ETH'],
+            'livecoin': ['BTM/BTC', 'BTM/ETH', 'NANO/BTC', 'NANO/ETH', 'XTZ/BTC', 'XTZ/ETH', 'THETA/BTC', 'THETA/ETH', 'ABBC/BTC', 'ABBC/ETH', 'AE/BTC', 'AE/ETH', 'IOST/BTC', 'IOST/ETH'],
             'theocean': [],
             # 'okex': ['AET/ETH','AET/BTC'],             # does not allow US, but allows canadian
             'bitmart': [],
             'cex': [],
-            'bitbay': [],
+            # 'bitbay': [],
             # 'bcex': [],             #candian exch, their API is updating
             'bitbay': [],
             'paymium': [],
             'binance': [],
             'okcoin': [],
             'bitfinex': []
-            }
+        }
         self.commonTickers = self.getCommonTickers()
         # then only call fetch_tickers for common_tickers between exchanges
         self.minProfit = 1  # percent profit
-        self.minVolume = 200 # in USD NOTE: still need to incorporate this. I think coinmarketcap API has a quick conversion call
+        # in USD NOTE: still need to incorporate this. I think coinmarketcap API has a quick conversion call
+        self.minVolume = 200
         self.txfrCosts = []
-
 
     def loadMarkets(self):
         print('___________Loading Markets______________')
@@ -60,39 +62,39 @@ class Arbitrager:
             try:
                 e.load_markets()
             except:
-                print(f'ccxt does not support {e} or {e} has updated their API')
+                print(
+                    f'ccxt does not support {e} or {e} has updated their API')
 
     def getCommonTickers(self):
         print('-----------getting common tickers---------------')
         tickers = {}
-        counter = 0     #only used in development 
+        counter = 0  # only used in development
         for key, e in self.exchanges.items():
             print(key)
             for symbol in list(e.markets.keys()):
-                # if (counter >80):      #only used in development
+                # if (counter >580):      #only used in development
                 #     break
                 if (symbol not in self.unavailableTickers[key]):
                     if symbol in tickers:
-                        counter +=1
+                        counter += 1
                         tickers[symbol].append(key)
                     else:
                         tickers[symbol] = [key]
 
-        #keep the tickers that show up in more than 1 exchange
+        # keep the tickers that show up in more than 1 exchange
         retTickers = {}
-        for key,val in tickers.items():
+        for key, val in tickers.items():
             if (len(val) > 1):
                 retTickers[key] = val
         return retTickers
 
-
     def startArbitrage(self):
         print('--------------Starting Arbitrage-------------------------')
         self.currentBooks = self.getAllpairPrices()
-        #calculate arb percent gain
+        # calculate arb percent gain
         self.ArbOpps2Way = self.findOpportunities()
         print(self.ArbOpps2Way)
-        self.ArbOpps2Way.sort(reverse=True, key=lambda el: el['maxProfit'])
+        self.ArbOpps2Way.sort(reverse=True, key=lambda el: el['pctProfit'])
         print(self.ArbOpps2Way)
         print('_______________')
         try:
@@ -101,7 +103,6 @@ class Arbitrager:
             print('did not find any opportunities')
         # after getting all the data, need to re-call APIs to get order book for top arb contenders
         # so we have most updated orders, calling all the available pairs took 3 minutes, coinbase throttled back also
-
 
     def getAllpairPrices(self):
         print('----------get all prices--------------')
@@ -118,17 +119,20 @@ class Arbitrager:
 
     def findOpportunities(self):
         print('----------------finding opps--------------')
-        opps = []         # this is a list of objs. each list is {highBid: {exch_name, buy_price, buy_amt}, lowAsk:  {exch_name, sell_price, sell_amt}, max_profit: number}
+        # this is a list of objs. each list is {highBid: {exch_name, buy_price, buy_amt}, lowAsk:  {exch_name, sell_price, sell_amt}, max_profit: number}
+        opps = []
         for tick in self.currentBooks:
             highesBid = {"exchName": None, "buyPrice": 0, "buyAmt": 0}
-            lowestAsk = {"exchName": None, "sellPrice": 9999999999, "sellAmt":0 }
+            lowestAsk = {"exchName": None,
+                         "sellPrice": 9999999999, "sellAmt": 0}
 
             for e in self.currentBooks[tick]:
                 # make sure there is more than 2 open orders
                 if (len(self.currentBooks[tick][e]['bids']) > 2 and len(self.currentBooks[tick][e]['asks']) > 2):
                     # looking at second best bid and second best ask and put highest buy and lowest sell exchanges in the 2 variable defined
-                    eBid = self.currentBooks[tick][e]['bids'][1][0]     
-                    eAsk = self.currentBooks[tick][e]['asks'][1][0]     #NOTE: these disregard volume best volume and ask/bid price
+                    eBid = self.currentBooks[tick][e]['bids'][1][0]
+                    # NOTE: these disregard volume best volume and ask/bid price
+                    eAsk = self.currentBooks[tick][e]['asks'][1][0]
                     if (eBid > highesBid['buyPrice']):
                         highesBid = {
                             "exchName": e, "buyPrice": eBid,
@@ -140,18 +144,37 @@ class Arbitrager:
                             "sellAmt": self.currentBooks[tick][e]['asks'][1][1]
                         }
 
-            #in case could not get orderbook for more than 1 exchange, or bid/asd didn't update
-            if ((lowestAsk['sellPrice'] != 9999999999) and (highesBid['buyPrice'] != 0)): 
+            # in case could not get orderbook for more than 1 exchange, or bid/asd didn't update
+            if ((lowestAsk['sellPrice'] != 9999999999) and (highesBid['buyPrice'] != 0)):
                 # check if profit above threshhold
-                diffPercentage = (highesBid['buyPrice'] - lowestAsk['sellPrice'])/highesBid['buyPrice']*100 
+                diffPercentage = (
+                    highesBid['buyPrice'] - lowestAsk['sellPrice'])/highesBid['buyPrice']*100
                 if (diffPercentage > self.minProfit):
                     opps.append({
                         'ticker': tick,
                         'highBid': highesBid,
                         'lowAsk': lowestAsk,
-                        'maxProfit': diffPercentage
+                        'pctProfit': diffPercentage,
+                        'maxProfit': self.getActualProfit(tick, highesBid['exchName'], lowestAsk['exchName']) if diffPercentage > 0 else 0
                     })
+
         return opps
+
+    def getActualProfit(self, ticker, bidExch, askExch):
+        bidsAvailable = copy.deepcopy(self.currentBooks[ticker][bidExch]['bids'])
+        asksAvialable = copy.deepcopy(self.currentBooks[ticker][askExch]['asks'])
+        profit = 0
+        #orderbook (bidsavailable and asksavailable) is list of touples, 1st pram is price, 2nd param is volume
+        #this is a simulation of buying/selling until no more arb opportunity, to calc profit
+        while (bidsAvailable[0][0] > asksAvialable[0][0]):
+            volumeToTrade = min(bidsAvailable[0][1], asksAvialable[0][1])
+            profit += volumeToTrade * (bidsAvailable[0][0]-asksAvialable[0][0])
+            bidsAvailable[0][1] -= volumeToTrade
+            asksAvialable[0][1] -= volumeToTrade
+            bidsAvailable.pop(0) if bidsAvailable[0][1] == 0 else asksAvialable.pop(0)
+            
+        return profit
+            
 
     def verifyOrderBook(self, ticker, exchHighBid, exchLowAsk):
         try:
@@ -167,7 +190,7 @@ class Arbitrager:
     def placeOrder(self, ticker, exch, amt):
         # place order
         pass
-    
+
     def verifyBalance(self, ticker, exch):
         try:
             balance = self.exchanges[exch].fetch_balance()
@@ -176,7 +199,7 @@ class Arbitrager:
         except:
             print('----- could not verify balance in {exch}')
 
-    def tradeOnOpps(self, opp = None):
+    def tradeOnOpps(self, opp=None):
         if (opp == None):
             if (len(self.ArbOpps2Way) > 0):
                 opp = self.ArbOpps2Way[0]
@@ -187,9 +210,9 @@ class Arbitrager:
         bidExch = opp['highBid']['exchName']
         askExch = opp['lowAsk']['exchName']
         if (self.verifyOrderBook(opp['ticker'], bidExch, askExch) != False):
-        # calc tx/trading costs
+            # calc tx/trading costs
             txCosts = self.getTradingAndTransactionCosts(
-                opp['ticker'], bidExch, askExch)  #default this to .001 BTC
+                opp['ticker'], bidExch, askExch)  # default this to .001 BTC
             if (txCosts < 0):
                 print(f"------could not get tx costs for {opp['ticker']}")
                 return
@@ -204,27 +227,24 @@ class Arbitrager:
             self.exchanges[bidExch].secret = config[bidExch]['API_SECRET']
 
             # verify funds in account
-            funds = self.verifyBalance(opp['ticker'],askExch)
-            if (funds > maxProfitTradeAmt/maxProfitAskPrice):  
-                self.placeOrder(opp['ticker'],askExch,maxProfitTradeAmt)
-                #verify trade executed
-                #transfer funds
-                #sell on high bid exch
-            
+            funds = self.verifyBalance(opp['ticker'], askExch)
+            if (funds > maxProfitTradeAmt/maxProfitAskPrice):
+                self.placeOrder(opp['ticker'], askExch, maxProfitTradeAmt)
+                # verify trade executed
+                # transfer funds
+                # sell on high bid exch
+
         else:
             print('-----missed opportunity--------')
             # maybe ask user to try again or try next opportunity
             return
-        
+
 
 arbitObj = Arbitrager()
 arbitObj.startArbitrage()
 # arbitObj.tradeOnOpps()
 
-
-
-
-#Previous Opps:
+# Previous Opps:
 # BTM/BTC bittrex to livecoin
 # NANO/BTC and NANO/ETH kraken to livecoin
 
@@ -232,40 +252,39 @@ arbitObj.startArbitrage()
 # • fetchMarkets (): Fetches a list of all available markets from an exchange and returns an array of markets
 # (objects with properties such as symbol, base, quote etc.). Some exchanges do not have means for obtaining
 # a list of markets via their online API. For those, the list of markets is hardcoded.
-# 
+#
 # • fetchCurrencies (): Fetches all available currencies an exchange and returns an associative dictionary of
 # currencies (objects with properties such as code, name, etc.). Some exchanges do not have means for obtaining
 # currencies via their online API. For those, the currencies will be extracted from market pairs or hardcoded.
-# 
+#
 # • loadMarkets ([reload]): Returns the list of markets as an object indexed by symbol and caches it with
 # the exchange instance. Returns cached markets if loaded already, unless the reload = true flag is forced.
-# 
+#
 # • fetchOrderBook (symbol[, limit = undefined[, params = {}]]): Fetch L2/L3 order
 # book for a particular market trading symbol.
-# 
+#
 # • fetchStatus ([, params = {}]): Returns information regarding the exchange status from either the
 # info hardcoded in the exchange instance or the API, if available.
-# 
+#
 # • fetchL2OrderBook (symbol[, limit = undefined[, params]]): Level 2 (priceaggregated) order book for a particular symbol.
-# 
+#
 # • fetchTrades (symbol[, since[, [limit, [params]]]]): Fetch recent trades for a particular
 # trading symbol.
-# 
+#
 # • fetchTicker (symbol): Fetch latest ticker data by trading symbol.
 
 # NOTE: use fetch_tickers() to get all ticker data. Some currencies, like Gemini Don't allow it
 
 
 
-
 # prices format
 # {
 #   'ADA/USD': {
-#     ccxt.binanceus(): {
+#     'binanceus': {
 #       'bids': [
-#         [0.0351, 30469.9],
-#         [0.035, 92924.9],
-#         [0.0349, 31921.3],
+#         [0.0357, 30469.9],
+#         [0.0356, 92924.9],
+#         [0.0351, 31921.3],
 #         [0.0348, 40142.7],
 #       ],
 #       'asks': [
@@ -278,7 +297,7 @@ arbitObj.startArbitrage()
 #       'datetime': None,
 #       'nonce': 20533044
 #     },
-#     ccxt.bittrex(): {
+#     'bittrex': {
 #       'bids': [
 #         [0.0351, 31753.01909686],
 #         [0.03501, 819.230988],
@@ -287,8 +306,8 @@ arbitObj.startArbitrage()
 #         [0.0181, 18853.59116022]
 #       ],
 #       'asks': [
-#         [0.0352, 31795.302],
-#         [0.03534, 291.556],
+#         [0.035, 31795.302],
+#         [0.0351, 291.556],
 #         [0.03548, 28421.0],
 #         [0.0355, 3457.08376422],
 #         [0.03551, 175241.0],
@@ -334,6 +353,4 @@ arbitObj.startArbitrage()
 #     }
 #   }
 # }
-
-
 
